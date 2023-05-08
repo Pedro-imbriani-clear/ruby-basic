81232aa346ca76cc3a9cc7d060932636e2ce378a
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-05-08 16:24:57 -0300

    Relational and Arithmetic Operators

---
+++ modulo2/04_relacionais.rb
@@ -0,0 +1,23 @@
+if 1 != 2 
+    puts 7 % 2
+else 
+    puts 'b'
+end
+if 1 < 2 
+    puts 'a'
+else 
+    puts 'b'
+end
+puts '----------------'
+if 1 > 2 
+    puts 'a'
+else 
+    puts 'b'
+end
+puts '----------------'
+if 1 != 2 
+    puts 'a'
+else 
+    puts 'b'
+end
+puts '----------------'
