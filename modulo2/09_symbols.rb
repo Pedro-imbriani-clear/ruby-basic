bff1d5059be6029dc9ebd559b72eae684fdfd2cb
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-05-08 16:27:35 -0300

    Symbols

---
+++ modulo2/09_symbols.rb
@@ -0,0 +1,11 @@
+puts 'Pedro'.object_id
+puts 'Pedro'.object_id
+puts 'Pedro'.object_id
+puts 'Pedro'.object_id
+puts '-----------------'
+
+puts :Pedro.object_id
+puts :Pedro.object_id
+puts :Pedro.object_id
+puts :Pedro.object_id
+puts :Pedro.object_id
